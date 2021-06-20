f5a5d14700aa75adb4e518dfe449b98dc1ba9a9e
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-06-20 11:49:51 -0400

    Add submit button

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -4,14 +4,16 @@ import { AvatarModule } from 'primeng/avatar';
 import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
   exports: [
     AvatarModule,
+    ButtonModule,
     DropdownModule,
+    InputTextModule,
     TableModule,
-    InputTextModule
   ]
 })
 export class PrimengModule { }
