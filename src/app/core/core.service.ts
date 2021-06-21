dcbbebcb2c0de0b0193768c991fdf7160f7fa521
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-06-21 11:51:55 -0400

    Add header in coreService and add welcome message

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
+import { ErrorHandlerService } from '../github/error-handler.service';
+import { GithubService } from '../github/github.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +12,25 @@ import { map } from "rxjs/operators";
 export class CoreService {
 
   public baseUrl: string = 'https://api.github.com';
+  public headers = new HttpHeaders({
+    'Accept': 'application/vnd.github.v3+json',
+  });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private errorHandler: ErrorHandlerService,
+              private githubService: GithubService) { }
 
   get_user(user: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/search/users?q=${user}`)
-    .pipe(map((res: any) => res.items[0]));
+    let username = user;
+    if (this.githubService.username) {
+      username = this.githubService.username
+    }    
+    return this.http.get(`${this.baseUrl}/search/users?q=${username}`,
+    { headers: this.headers })
+    .pipe(
+      map((res: any) => res.items[0]),
+      catchError(this.errorHandler.handleError)
+    );
   }
   
 }
